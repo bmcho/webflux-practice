20f97011336ac4bbdd23ce7d3cd2851e3c1bfcb8
bmcho <bmcho91@naver.com>
2025-07-23 11:07:40 +0900

    add: router function 간단 구현

--- src/main/java/com/bmcho/webfluxpratice/practice/webflux/RouterFunctionExample.java
+++ src/main/java/com/bmcho/webfluxpratice/practice/webflux/RouterFunctionExample.java
@@ -2,16 +2,18 @@ package com.bmcho.webfluxpratice.practice.webflux;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.netty.http.server.HttpServer;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Slf4j
 public class RouterFunctionExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         log.info("start");
 
         RouterFunction<ServerResponse> router = RouterFunctions.route()
@@ -34,6 +36,16 @@ public class RouterFunctionExample {
             )
             .build();
 
+        var httpHandler = RouterFunctions.toHttpHandler(router);
+        var adapter = new ReactorHttpHandlerAdapter(httpHandler);
+
+        HttpServer.create()
+            .host("localhost")
+            .port(8080)
+            .handle(adapter)
+            .bindNow()
+            .channel().closeFuture().sync();
     }
+
 
 }
