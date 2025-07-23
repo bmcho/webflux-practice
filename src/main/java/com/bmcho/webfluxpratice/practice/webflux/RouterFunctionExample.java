3b80258895140a8dec2643c9f383071c8d55c30c
bmcho <bmcho91@naver.com>
2025-07-23 11:10:54 +0900

    add: router function 간단 구현

--- src/main/java/com/bmcho/webfluxpratice/practice/webflux/RouterFunctionExample.java
+++ src/main/java/com/bmcho/webfluxpratice/practice/webflux/RouterFunctionExample.java
@@ -1,11 +1,15 @@
 package com.bmcho.webfluxpratice.practice.webflux;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.server.HttpServer;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
@@ -47,5 +51,54 @@ public class RouterFunctionExample {
             .channel().closeFuture().sync();
     }
 
+
+    @Slf4j
+    public static class GreetingHandler {
+        public static Mono<ServerResponse> greetQueryParam(ServerRequest serverRequest) {
+            String name = serverRequest.queryParam("name")
+                .orElse("world");
+
+            String content = "Hello " + name;
+            return ServerResponse.ok().bodyValue(content);
+        }
+
+        public static Mono<ServerResponse> greetPathVariable(ServerRequest serverRequest) {
+            String name = serverRequest.pathVariable("name");
+
+            String content = "Hello " + name;
+            return ServerResponse.ok().bodyValue(content);
+        }
+
+        public static Mono<ServerResponse> greetJsonBody(ServerRequest serverRequest) {
+            return serverRequest.bodyToMono(NameHolder.class)
+                .map(NameHolder::getName)
+                .map(name -> "Hello " + name)
+                .flatMap(content -> ServerResponse.ok().bodyValue(content))
+                .doOnError(throwable -> log.error("error", throwable));
+        }
+
+        public static Mono<ServerResponse> greetPlainTextBody(ServerRequest serverRequest) {
+            return serverRequest.bodyToMono(String.class)
+                .map(name -> "Hello " + name)
+                .flatMap(content -> ServerResponse.ok().bodyValue(content));
+        }
+
+        public static Mono<ServerResponse> greetHeader(ServerRequest serverRequest) {
+            String name = serverRequest.headers().header("X-Custom-Name")
+                .stream()
+                .findFirst()
+                .orElse("world");
+
+            String content = "Hello " + name;
+            return ServerResponse.ok().bodyValue(content);
+        }
+    }
+
+
+    @Setter
+    @Getter
+    public static class NameHolder {
+        private String name = "";
+    }
 
 }
